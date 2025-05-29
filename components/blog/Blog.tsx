c8e5686c37e793346c66eae2e552040cab5592ba
Hazama32 <bintanghack32@gmail.com>
2025-05-29 15:06:12 +0700

    tambah animasi blog

--- components/blog/Blog.tsx
+++ components/blog/Blog.tsx
@@ -8,16 +8,16 @@ import Artikel3 from '@/components/blog/Artikel3';
 import Artikel4 from '@/components/blog/Artikel4';
 
 export default function Blog() {
-     const { ref } = useSectionInView("Blog");
+     const { ref } = useSectionInView("Blog",0.3);
 
   return (
-       <motion.section
-            ref={ref}
-            className="mb-28 max-w-[45rem] text-left leading-8 sm:mb-40 scroll-mt-28"
+      <motion.section
+        id="blog"
+        ref={ref}
+        className="min-h-screen scroll-mt-28"
             initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.175 }}
-            id="blog"
           >
     <main className="p-6 max-w-3xl mx-auto">
       <motion.h1
